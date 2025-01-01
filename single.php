a3af8ffaca438afc20fef597936de773959a969a
ledihduc <anhledinhduc2596@gmail.com>
2025-01-01 21:27:48 +0700

    add next next and prev post

--- single.php
+++ single.php
@@ -11,6 +11,12 @@ $url = get_field('blog_image', $post->ID);
 
 $author_id = $post->post_author; // Get author ID
 $author_name = get_the_author_meta('display_name', $author_id);
+
+// Get previous and next posts
+$prev_post = get_previous_post();
+$next_post = get_next_post();
+// print_r($prev_post);
+// print_r($next_post);
 ?>
 
 <style>
@@ -46,22 +52,30 @@ $author_name = get_the_author_meta('display_name', $author_id);
     </div>
     <div class="w-full h-[1px] bg-[#D9D9D9] my-16"></div>
     <div class="flex text-white items-center justify-between w-full gap-4">
-      <div class="flex items-center gap-4">
+      <div class="w-5/12">
+        <?php if($next_post): ?>
+        <a href="<?php echo $next_post->guid; ?>" class="flex items-center justify-start gap-4">
         <div
           class="flex shrink-0 bg-blue-900 rounded-full h-[35px] w-[35px] p-2"
         >
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/svg/arrow-white.svg" alt="arrow" srcset="">
+          <img class="rotate-180" src="<?php echo get_template_directory_uri(); ?>/assets/svg/arrow-white.svg" alt="arrow" srcset="">
         </div>
-        <p>タイトルが入ります。タイトルが入ります。</p>
+        <p class="truncate"><?php echo $next_post->post_title; ?></p>
+        </a>
+        <?php endif; ?>
       </div>
-      <div class="bg-white rounded-full py-[14px] px-3"><img src="<?php echo get_template_directory_uri(); ?>/assets/svg/menu.svg ?>" ></div>
-      <div class="flex items-center gap-4">
-        <p>タイトルが入ります。タイトルが入ります。</p>
+      <a href="<?php echo esc_url(home_url('/news')); ?>" class="bg-white rounded-full py-[14px] px-3"><img src="<?php echo get_template_directory_uri(); ?>/assets/svg/menu.svg ?>" ></a>
+      <div class="w-5/12">
+        <?php if($prev_post): ?>
+        <a href="<?php echo $prev_post->guid; ?>" class="flex items-center justify-end gap-4">
+        <p class="truncate"><?php echo $prev_post->post_title; ?></p>
         <div
           class="flex shrink-0 bg-blue-900 rounded-full h-[35px] w-[35px] p-2"
         >
           <img src="<?php echo get_template_directory_uri(); ?>/assets/svg/arrow-white.svg" alt="arrow" srcset="">
         </div>
+        </a>
+        <?php endif; ?>
       </div>
     </div>
   </div>

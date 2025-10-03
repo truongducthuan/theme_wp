012fbe797274cdbfca4b0ef6d9b5e06d060e1bb2
Tung Kieu <tungka@nal.vn>
2025-10-03 17:14:26 +0700

    service page

---
+++ template-parts/services/hero-section.php
@@ -0,0 +1,86 @@
+<section class="w-full bg-white">
+  <div class="mx-auto w-full pt-24 bg-white max-w-7xl text-[#222627]">
+    <div class="w-full">
+      <img
+        src="http://synora-home.local/wp-content/uploads/2025/10/services-home-scaled.png"
+        alt="Digital transformation process visualization"
+        class="w-full h-auto shadow-2xl object-cover" />
+    </div>
+
+    <div class="w-full flex flex-col items-center justify-center bg-white mt-28 pb-16">
+      <div class="w-[850px] h-12 flex flex-row justify-between items-center gap-2.5 mb-7">
+        <div class="w-6">
+          <img
+            src="http://synora-home.local/wp-content/uploads/2025/10/service-icon-header.png"
+            alt="Icon"
+            class="w-full h-auto object-cover" />
+        </div>
+        <h1 class="flex-1 text-center text-[40px] text-[#2F2BEB] font-bold">私たちのサービス - 観光DX開発支援</h1>
+        <div class="w-6">
+          <img
+            src="http://synora-home.local/wp-content/uploads/2025/10/service-icon-header.png"
+            alt="Icon"
+            class="w-full h-auto object-cover" />
+        </div>
+      </div>
+      <h3 class="text-center text-xl text-black font-bold">私たちは単なるソフトウェア会社ではなく、観光業界に特化した伴走型の技術パートナーです。</h3>
+    </div>
+
+    <div class="w-full grid grid-cols-3 gap-1 px-3 pb-16">
+      <div class="min-h-36 flex flex-col gap-y-6 bg-slate-400 px-3">
+        <div class="rounded-[20px] py-10 pl-8 pr-4 bg-[#F5F4FF] relative">
+          <div class="w-10 h-10 absolute right-2.5 top-2.5 rounded-full bg-[#2BC8EB] text-black text-xl font-bold flex items-center justify-center">01</div>
+          <div class="flex gap-4 items-center mb-6">
+            <img
+              src="http://synora-home.local/wp-content/uploads/2025/10/icon-technological.png"
+              alt="Icon"
+              class="w-[70px] h-auto object-cover" />
+            <h4 class="flex-1 text-xl font-bold">増え続ける技術的複雑性</h4>
+          </div>
+          <p class="text-sm text-[#616669]">観光業界の複雑な商慣習や業務フローを理解し、それをシステムに落とし込む作業は、チームに高い専門性を要求し、新規メンバーのキャッチアップを困難にします。</p>
+        </div>
+        <div class="rounded-[20px] py-10 pl-8 pr-4 bg-[#F5F4FF] relative">
+          <div class="w-10 h-10 absolute right-2.5 top-2.5 rounded-full bg-[#2BC8EB] text-black text-xl font-bold flex items-center justify-center">02</div>
+          <div class="flex gap-4 items-center mb-6">
+            <img
+              src="http://synora-home.local/wp-content/uploads/2025/10/icon-domain.png"
+              alt="Icon"
+              class="w-[70px] h-auto object-cover" />
+            <h4 class="flex-1 text-lg font-bold">求められる高度なドメイン知識</h4>
+          </div>
+          <p class="text-sm text-[#616669]">観光業界の複雑な商慣習や業務フローを理解し、それをシステムに落とし込む作業は、チームに高い専門性を要求し、新規メンバーのキャッチアップを困難にします。</p>
+        </div>
+      </div>
+      <div class="min-h-36 flex items-center justify-center">
+        <img
+          src="http://synora-home.local/wp-content/uploads/2025/10/meeting-zoom.png"
+          alt="Icon"
+          class="w-[376px] h-auto object-cover" />
+      </div>
+      <div class="min-h-36 flex flex-col gap-y-6 bg-slate-400 px-3">
+        <div class="rounded-[20px] py-10 pl-8 pr-4 bg-[#F5F4FF] relative">
+          <div class="w-10 h-10 absolute right-2.5 top-2.5 rounded-full bg-[#2BC8EB] text-black text-xl font-bold flex items-center justify-center">03</div>
+          <div class="flex gap-4 items-center mb-6">
+            <img
+              src="http://synora-home.local/wp-content/uploads/2025/10/icon-teams.png"
+              alt="Icon"
+              class="w-[70px] h-auto object-cover" />
+            <h4 class="flex-1 text-xl font-bold">内製チーム強化の必要性</h4>
+          </div>
+          <p class="text-sm text-[#616669]">ビジネスのスピードを上げるには、開発チームの内製化が理想です。しかし、優秀な人材の採用は困難を極め、自社だけでチームをスケールさせることには限界があります。</p>
+        </div>
+        <div class="rounded-[20px] py-10 pl-8 pr-4 bg-[#F5F4FF] relative">
+          <div class="w-10 h-10 absolute right-2.5 top-2.5 rounded-full bg-[#2BC8EB] text-black text-xl font-bold flex items-center justify-center">04</div>
+          <div class="flex gap-4 items-center mb-6">
+            <img
+              src="http://synora-home.local/wp-content/uploads/2025/10/icon-domain.png"
+              alt="Icon"
+              class="w-[70px] h-auto object-cover" />
+            <h4 class="flex-1 text-lg font-bold">恒常的なリソースの制約</h4>
+          </div>
+          <p class="text-sm text-[#616669]">既存システムの運用保守や、細かな改修依頼に対応する中で、次世代プロダクトの開発や、大規模なリファクタリングといった、重要だが緊急ではないタスクが後回しになりがちです。</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ templates/services.php
@@ -0,0 +1,16 @@
+<?php
+// Template Name: Services
+get_header();
+?>
+
+<?php
+// Hero Section
+get_template_part('template-parts/services/hero-section');
+
+// Blogs Section
+// get_template_part('template-parts/blogs/blogs-section');
+?>
+
+<?php
+get_footer();
+?>

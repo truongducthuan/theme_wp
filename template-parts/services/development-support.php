6d403c802cf83144b5706e1a1bef23f9a3033b44
Tung Kieu <tungka@nal.vn>
2025-10-06 11:57:05 +0700

    fix image error

---
+++ template-parts/services/development-support.php
@@ -0,0 +1,87 @@
+<style>
+  .news-hero__background {
+    position: relative;
+    background-image: url('<?php echo get_site_url(); ?>/wp-content/uploads/2025/10/image-team-work.png');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    overflow: hidden;
+  }
+
+  .news-hero__background::before {
+    content: "";
+    position: absolute;
+    inset: 0;
+    background-color: rgba(255, 255, 255, 0.7);
+    backdrop-filter: blur(2px);
+    z-index: 1;
+  }
+
+  .news-hero__background>* {
+    position: relative;
+    z-index: 2;
+  }
+
+</style>
+
+<section class="news-hero__background pb-20">
+  <?php get_template_part('template-parts/services/common/header-title', null, ['title' => '貴社に最適化された柔軟な「開発支援」', 'sub-title' => 'コンセプト策定からフルスクラッチ開発、グロース支援まで、貴社のビジネスに最適な観光DXをワンストップでご提供。']); ?>
+
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto px-4">
+    <!-- Card 1 -->
+    <div class="h-[450px] bg-[#2BC8EB] rounded-tr-[100px] shadow-md hover:shadow-xl transition-shadow duration-300">
+      <div class="w-full h-full bg-white rounded-tr-[150px] p-8 flex flex-col items-center text-center pb-28 ">
+        <div class="w-16 h-16 mb-6 flex items-center justify-center bg-[#2F2BEB] rounded-[10px]">
+          <img alt="icon" src="<?php echo get_template_directory_uri() . '/assets/imgs/svgs/icon-setting-pc.svg'; ?>" />
+        </div>
+        <h3 class="text-lg font-bold mb-4 text-[#222627]">ラボ型開発支援（チーム拡張）</h3>
+        <button
+          class="mt-auto bg-[#2F2BEB] text-white text-sm font-semibold px-6 py-2 rounded-full hover:bg-[#1c1bbd] transition-colors duration-300">
+          もっと詳しく
+        </button>
+      </div>
+    </div>
+
+    <!-- Card 2 -->
+    <div class="h-[450px] bg-[#2BC8EB] rounded-tr-[100px] shadow-md hover:shadow-xl transition-shadow duration-300">
+      <div class="w-full h-full bg-white rounded-tr-[150px] p-8 flex flex-col items-center text-center pb-28 ">
+        <div class="w-16 h-16 mb-6 flex items-center justify-center bg-[#2F2BEB] rounded-[10px]">
+          <img alt="icon" src="<?php echo get_template_directory_uri() . '/assets/imgs/svgs/cloud-icon-white.svg'; ?>" />
+        </div>
+        <h3 class="text-lg font-bold mb-4 text-[#222627]">ラボ型開発支援（チーム拡張）</h3>
+        <button
+          class="mt-auto bg-[#2F2BEB] text-white text-sm font-semibold px-6 py-2 rounded-full hover:bg-[#1c1bbd] transition-colors duration-300">
+          もっと詳しく
+        </button>
+      </div>
+    </div>
+
+    <!-- Card 3 -->
+    <div class="h-[450px] bg-[#2BC8EB] rounded-tr-[100px] shadow-md hover:shadow-xl transition-shadow duration-300">
+      <div class="w-full h-full bg-white rounded-tr-[150px] p-8 flex flex-col items-center text-center pb-28 ">
+        <div class="w-16 h-16 mb-6 flex items-center justify-center bg-[#2F2BEB] rounded-[10px]">
+          <img alt="icon" src="<?php echo get_template_directory_uri() . '/assets/imgs/svgs/cloud-icon-white.svg'; ?>" />
+        </div>
+        <h3 class="text-lg font-bold mb-4 text-[#222627]">ラボ型開発支援（チーム拡張）</h3>
+        <button
+          class="mt-auto bg-[#2F2BEB] text-white text-sm font-semibold px-6 py-2 rounded-full hover:bg-[#1c1bbd] transition-colors duration-300">
+          もっと詳しく
+        </button>
+      </div>
+    </div>
+
+    <!-- Card 4 -->
+    <div class="h-[450px] bg-[#2BC8EB] rounded-tr-[100px] shadow-md hover:shadow-xl transition-shadow duration-300">
+      <div class="w-full h-full bg-white rounded-tr-[150px] p-8 flex flex-col items-center text-center pb-28 ">
+        <div class="w-16 h-16 mb-6 flex items-center justify-center bg-[#2F2BEB] rounded-[10px]">
+          <img alt="icon" src="<?php echo get_template_directory_uri() . '/assets/imgs/svgs/icon-protect-pc.svg'; ?>" />
+        </div>
+        <h3 class="text-lg font-bold mb-4 text-[#222627]">ラボ型開発支援（チーム拡張）</h3>
+        <button
+          class="mt-auto bg-[#2F2BEB] text-white text-sm font-semibold px-6 py-2 rounded-full hover:bg-[#1c1bbd] transition-colors duration-300">
+          もっと詳しく
+        </button>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ template-parts/services/service-contract.php
@@ -0,0 +1,55 @@
+<section class="mt-16 py-16 px-20 bg-[#F5F4FF] flex">
+  <!-- left -->
+  <div class="w-2/5">
+    <div class="w-full flex flex-col items-start justify-center pb-10">
+      <div class="w-[180px] h-12 flex flex-row justify-between items-center gap-1 mb-1">
+        <div class="w-6">
+          <img alt="icon" src="<?php echo get_template_directory_uri() . '/assets/imgs/svgs/service-icon-header.svg'; ?>" />
+        </div>
+        <h3 class="flex-1 text-center text-lg text-[#2F2BEB] font-bold">提供サービス</h3>
+        <div class="w-6">
+          <img alt="icon" src="<?php echo get_template_directory_uri() . '/assets/imgs/svgs/service-icon-header.svg'; ?>" />
+        </div>
+      </div>
+      <p class=" text-black font-normal">DXを通じて観光産業を盛り上げるパートナーを募集しています。お気軽にお問い合わせください。</p>
+    </div>
+    <div class="flex flex-col gap-4">
+      <div class="w-full py-2 flex gap-5">
+        <div class="h-16 w-16 flex items-center justify-center bg-[#2F2BEB] rounded-[10px]">
+          <img class="w-8 h-auto" alt="icon" src="<?php echo get_template_directory_uri() . '/assets/imgs/svgs/phone-white.svg'; ?>" />
+        </div>
+        <span class="h-full flex-1 flex justify-center flex-col">
+          <p class="text-[#616669] font-bold">Call Us Any Time</p>
+          <p class="text-2xl font-bold">+(009) 1888 000 2222</p>
+        </span>
+      </div>
+      <div class="w-full py-2 flex gap-5">
+        <div class="h-16 w-16 flex items-center justify-center bg-[#2F2BEB] rounded-[10px]">
+          <img class="w-8 h-auto" alt="icon" src="<?php echo get_template_directory_uri() . '/assets/imgs/svgs/mail-white.svg'; ?>" />
+        </div>
+        <span class="h-full flex-1 flex justify-center flex-col">
+          <p class="text-[#616669] font-bold">Call Us Any Time</p>
+          <p class="text-2xl font-bold">+(009) 1888 000 2222</p>
+        </span>
+      </div>
+      <div class="w-full py-2 flex gap-5">
+        <div class="h-16 w-16 flex items-center justify-center bg-[#2F2BEB] rounded-[10px]">
+          <img class="w-8 h-auto" alt="icon" src="<?php echo get_template_directory_uri() . '/assets/imgs/svgs/map-white.svg'; ?>" />
+        </div>
+        <span class="h-full flex-1 flex justify-center flex-col">
+          <p class="text-[#616669] font-bold">Call Us Any Time</p>
+          <p class="text-2xl font-bold">+(009) 1888 000 2222</p>
+        </span>
+      </div>
+    </div>
+  </div>
+
+  <!-- right -->
+  <div class="w-full flex  flex-1 items-center gap-6 px-3 pb-16">
+    <img
+      src="<?php echo get_site_url() . '/wp-content/uploads/2025/10/ef6c4dd98615d6be4dbfae7f79e5af47d82ecf21.jpg' ?>"
+      alt="Icon"
+      class="w-full h-auto object-cover rounded-[30px]" />
+  </div>
+
+</section>
